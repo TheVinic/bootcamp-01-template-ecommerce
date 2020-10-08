9c47bf0a5b04ba5f95166cfd0aabc4a81cabbf5f
Vinicius Nunes Moura <vinicnm@gmail.com>
2020-10-08 14:30:21 -0300

    Spring initializer

---
+++ src/main/java/com/itau/ecom/Bootcamp01TemplateEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.itau.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp01TemplateEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp01TemplateEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itau/ecom/Bootcamp01TemplateEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootcamp01TemplateEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
